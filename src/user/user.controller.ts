29bb2328cae6ddf16e3f79525aa3b8c47ae53fc6
solidados <geekbrains.pk@internet.ru>
2024-11-09 17:20:14 +0400

    feat: create User controller

---
+++ src/user/user.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Param,
+  Controller,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/createUser.dto';
+import { UpdateUserDto } from './dto/updateUser.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(Number(id));
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(Number(id), updateUserDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.userService.delete(Number(id));
+  }
+}
