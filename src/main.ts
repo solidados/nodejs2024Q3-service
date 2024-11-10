d667e53385070fc4ca46dd9094afc4c237665b13
solidados <geekbrains.pk@internet.ru>
2024-11-10 10:45:51 +0400

    chore: update main: add ValidationPipe of nest

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 const PORT = Number(process.env.PORT) || 4001;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT, () =>
     console.log(
       `\x1b[96mServer is running on PORT: \x1b[7m ${PORT} \x1b[27m\x1b[0m`,
