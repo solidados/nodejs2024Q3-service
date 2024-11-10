4437536b38c663bd41955851e737ec711eb44936
solidados <geekbrains.pk@internet.ru>
2024-11-10 19:44:34 +0400

    feat: create Artist controller

---
+++ src/artist/artist.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { ArtistService } from './artist.service';
+import { CreateArtistDto } from './dto/createArtist.dto';
+import { Artist } from './entities/artist.entity';
+import { UpdateArtistDto } from './dto/updateArtist.dto';
+
+@Controller('artist')
+export class ArtistController {
+  constructor(private readonly artistService: ArtistService) {
+    this.artistService = artistService;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() createArtistDto: CreateArtistDto) {
+    return this.artistService.create(createArtistDto);
+  }
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll(): Artist[] {
+    return this.artistService.findAll();
+  }
+
+  @Get(':id')
+  @HttpCode(HttpStatus.OK)
+  findOne(@Param('id', ParseUUIDPipe) id: string): Artist {
+    return this.artistService.findOne(id);
+  }
+
+  @Put(':id')
+  @HttpCode(HttpStatus.OK)
+  update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() updateArtistDTO: UpdateArtistDto,
+  ): Artist {
+    return this.artistService.update(id, updateArtistDTO);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id', new ParseUUIDPipe()) id: string) {
+    return this.artistService.delete(id);
+  }
+}
