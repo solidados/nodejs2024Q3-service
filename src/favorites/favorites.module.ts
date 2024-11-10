9677019ec00544a469dace838047a1275a150a12
solidados <geekbrains.pk@internet.ru>
2024-11-10 22:30:49 +0400

    feat: create Favorites @Module

---
+++ src/favorites/favorites.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../database/database.module';
+import { FavoritesController } from './favorites.controller';
+import { FavoritesService } from './favorites.service';
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [FavoritesController],
+  providers: [FavoritesService],
+})
+export class FavoritesModule {}
