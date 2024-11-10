cceecb18b98a616d9889d80534e325a099b20da5
solidados <geekbrains.pk@internet.ru>
2024-11-10 22:32:12 +0400

    update: add FavoritesModule to AppModule imports array

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { UserModule } from './user/user.module';
 import { TrackModule } from './track/track.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
+import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { AlbumModule } from './album/album.module';
     TrackModule,
     ArtistModule,
     AlbumModule,
+    FavoritesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
