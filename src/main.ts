b117536287aac355f3635bf0ad7b94bd357052d6
solidados <geekbrains.pk@internet.ru>
2024-11-10 12:12:13 +0400

    chore: edit PORT in main.ts with nest/config service

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,21 @@
-import 'dotenv/config';
+// import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
-const PORT = Number(process.env.PORT) || 4001;
+// const PORT = Number(process.env.PORT) || 4001;
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+async function bootstrap(): Promise<void> {
+  const app: INestApplication = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT', 4001);
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(PORT, () =>
+
+  await app.listen(port, () =>
     console.log(
-      `\x1b[96mServer is running on PORT: \x1b[7m ${PORT} \x1b[27m\x1b[0m`,
+      `\x1b[96mServer is running on PORT: \x1b[7m ${port} \x1b[27m\x1b[0m`,
     ),
   );
 }
