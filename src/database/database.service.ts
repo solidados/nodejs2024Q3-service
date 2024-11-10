ec17e781bd66c2325fa52d40780a48a710ee9a40
solidados <geekbrains.pk@internet.ru>
2024-11-10 20:14:35 +0400

    chore: update database.service.ts with Album methods

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -2,12 +2,14 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { User } from '../user/entities/user.entity';
 import { Track } from '../track/entities/track.entity';
 import { Artist } from '../artist/entities/artist.entity';
+import { Album } from '../album/entities/album.entity';
 
 @Injectable()
 export class DatabaseService {
   private readonly users: User[] = [];
   private readonly tracks: Track[] = [];
   private readonly artists: Artist[] = [];
+  private readonly albums: Album[] = [];
 
   /** - `Add` block */
   addUser(user: User): void {
@@ -20,6 +22,10 @@ export class DatabaseService {
 
   addArtist(artist: Artist): void {
     this.artists.push(artist);
+  }
+
+  addAlbum(album: Album): void {
+    this.albums.push(album);
   }
 
   /** - `Get All` block */
@@ -35,6 +41,10 @@ export class DatabaseService {
     return this.artists;
   }
 
+  getAlbums(): Album[] {
+    return this.albums;
+  }
+
   /** - `Get One by ID` block */
   getUserById(id: string): User | undefined {
     return this.users.find((usr: User): boolean => usr.id === id);
@@ -48,13 +58,19 @@ export class DatabaseService {
     return this.artists.find((art: Artist): boolean => art.id === id);
   }
 
+  getAlbumById(id: string): Album | undefined {
+    return this.albums.find((alb: Album): boolean => alb.id === id);
+  }
+
   /** - `Update` block */
   updateUser(user: User): boolean {
     const index: number = this.users.findIndex(
       (usr: User): boolean => usr.id === user.id,
     );
     if (index === -1)
-      throw new NotFoundException(`User with ID ${user.id} not found.`);
+      throw new NotFoundException({
+        message: `User with ID ${user.id} not found.`,
+      });
     this.users[index] = user;
 
     return true;
@@ -65,7 +81,9 @@ export class DatabaseService {
       (trk: Track): boolean => trk.id === track.id,
     );
     if (index === -1)
-      throw new NotFoundException(`Track with ID ${track.id} not found.`);
+      throw new NotFoundException({
+        message: `Track with ID ${track.id} not found.`,
+      });
     this.tracks[index] = track;
 
     return true;
@@ -76,8 +94,23 @@ export class DatabaseService {
       (art: Artist): boolean => art.id === artist.id,
     );
     if (index === -1)
-      throw new NotFoundException(`Artist with ID ${artist.id} not found.`);
+      throw new NotFoundException({
+        message: `Artist with ID ${artist.id} not found.`,
+      });
     this.artists[index] = artist;
+
+    return true;
+  }
+
+  updateAlbum(album: Album): boolean {
+    const index: number = this.albums.findIndex(
+      (alb: Album): boolean => alb.id === album.id,
+    );
+    if (index === -1)
+      throw new NotFoundException({
+        message: `Album with ID ${album.id} not found.`,
+      });
+    this.albums[index] = album;
 
     return true;
   }
@@ -88,7 +121,7 @@ export class DatabaseService {
       (usr: User): boolean => usr.id === id,
     );
     if (index === -1)
-      throw new NotFoundException(`User with ID ${id} not found.`);
+      throw new NotFoundException({ message: `User with ID ${id} not found.` });
     this.users.splice(index, 1);
 
     return true;
@@ -99,7 +132,9 @@ export class DatabaseService {
       (trk: Track): boolean => trk.id === id,
     );
     if (index === -1)
-      throw new NotFoundException(`Track with ID ${id} not found.`);
+      throw new NotFoundException({
+        message: `Track with ID ${id} not found.`,
+      });
     this.tracks.splice(index, 1);
 
     return true;
@@ -110,12 +145,33 @@ export class DatabaseService {
       (art: Artist): boolean => art.id === id,
     );
     if (index === -1)
-      throw new NotFoundException(`Artist with ID ${id} not found.`);
+      throw new NotFoundException({
+        message: `Artist with ID ${id} not found.`,
+      });
 
     this.tracks.forEach((track: Track): void => {
       if (track.artistId === id) track.artistId = null;
     });
+
+    this.albums.forEach((album: Album): void => {
+      if (album.artistId === id) album.artistId = null;
+    });
+
     this.artists.splice(index, 1);
+
+    return true;
+  }
+
+  deleteAlbum(id: string): boolean {
+    const index: number = this.albums.findIndex(
+      (alb: Album): boolean => alb.id === id,
+    );
+    if (index === -1)
+      throw new NotFoundException({
+        message: `Album with ID ${id} not found.`,
+      });
+
+    this.albums.splice(index, 1);
 
     return true;
   }
