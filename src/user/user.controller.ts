f8b1985a8624207d6b861b4144537e5bb9df61d7
solidados <geekbrains.pk@internet.ru>
2024-11-11 15:01:18 +0400

    chore: start adding descriptions to response

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -14,23 +14,62 @@ import { UserService } from './user.service';
 import { CreateUserDto } from './dto/createUser.dto';
 import { UpdateUserDto } from './dto/updateUser.dto';
 import { User } from './entities/user.entity';
+import { ApiBody, ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiOperation({
+    summary: 'Create User',
+    description: 'create user (following DTO should be used) CreateUserDto',
+  })
+  @ApiBody({ type: CreateUserDto })
+  @ApiResponse({
+    status: 201,
+    description:
+      'Server should answer with status code 201 and newly created record if request is valid',
+  })
+  @ApiResponse({
+    status: 400,
+    description:
+      'Server should answer with status code 400 and corresponding message if request body does not contain required fields',
+  })
   @Post()
   @HttpCode(HttpStatus.CREATED)
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
+  @ApiOperation({ summary: 'Get all Users' })
+  @ApiResponse({
+    status: 200,
+    description:
+      'Server should answer with status code 200 and all users records',
+  })
   @Get()
   @HttpCode(HttpStatus.OK)
   findAll(): User[] {
     return this.userService.findAll();
   }
 
+  @ApiOperation({ summary: 'Get Single User By ID' })
+  @ApiParam({ name: 'id', description: 'User ID' })
+  @ApiResponse({
+    status: 200,
+    description:
+      'Server should answer with status code 200 and and record with id === userId if it exists',
+  })
+  @ApiResponse({
+    status: 400,
+    description:
+      'Server should answer with status code 400 and corresponding message if userId is invalid (not uuid)',
+  })
+  @ApiResponse({
+    status: 404,
+    description:
+      "Server should answer with status code 404 and corresponding message if record with id === userId doesn't exist",
+  })
   @Get(':id')
   @HttpCode(HttpStatus.OK)
   findOne(@Param('id', ParseUUIDPipe) id: string): User {
