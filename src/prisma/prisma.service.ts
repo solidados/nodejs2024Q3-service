a85cc4dbdf76b981794547fafdc7dde4396120eb
solidados <geekbrains.pk@internet.ru>
2024-11-15 17:12:01 +0400

    feat: configure dynamic database URL selection for local and Docker environments
    - Updated `.env` to include separate `DATABASE_URL_LOCAL` and `DATABASE_URL_DOCKER` variables.
    - Modified `prisma.service.ts` to dynamically select the database URL based on the environment.
    - Updated `docker-compose.yml` to use `DATABASE_URL_DOCKER` for the application container.
    - Defaulted `DATABASE_URL` to `DATABASE_URL_LOCAL` for local development.

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,18 @@
+import * as process from 'node:process';
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    await this.$connect();
+  async onModuleInit(): Promise<void> {
+    const databaseUrl: string =
+      process.env.DATABASE_URL || process.env.DATABASE_URL_LOCAL;
+    await this.$connect({
+      dataSourceSwitch: {
+        db: {
+          url: databaseUrl,
+        },
+      },
+    });
   }
 }
