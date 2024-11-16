a4dd1d913c860b2c2be5caa65cdd60f1ad871cd3
solidados <geekbrains.pk@internet.ru>
2024-11-16 14:01:03 +0400

    refactor: minor changes to Album: entity

--- src/album/entities/album.entity.ts
+++ src/album/entities/album.entity.ts
@@ -5,7 +5,7 @@ export class Album {
   name: string;
   year: number;
   artistId: string | null;
-  
+
   constructor(name: string, year: number, artistId: string | null = null) {
     this.id = uuidv4();
     this.name = name;
