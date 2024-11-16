bd924e9fbcca6f548d21aa5ef89ba425a1868a5e
solidados <geekbrains.pk@internet.ru>
2024-11-16 23:21:33 +0400

    chore: update prisma.service.ts

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,18 +1,16 @@
-import * as process from 'node:process';
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   async onModuleInit(): Promise<void> {
-    const databaseUrl: string =
-      process.env.DATABASE_URL || process.env.DATABASE_URL_LOCAL;
-    await this.$connect({
-      dataSourceSwitch: {
-        db: {
-          url: databaseUrl,
-        },
-      },
-    });
+    await this.$connect();
+  }
+
+  async onModuleDestroy(): Promise<void> {
+    await this.$disconnect();
   }
 }
