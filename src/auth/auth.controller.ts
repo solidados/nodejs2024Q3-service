8394ab1892aef2b291c39f4b27669a3918e9df8b
solidados <geekbrains.pk@internet.ru>
2024-11-21 15:25:52 +0400

    feat: create AuthController and AuthModule; set custom decorator for @Public()

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UsePipes,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Public } from './auth.public.decorator';
+import { AuthDto } from './dto/auth.dto';
+import { User } from '../user/entities/user.entity';
+import { Auth } from './entity/auth.entity';
+import { ValidationTokenPipe } from '../token/token.pipe';
+import { RefreshTokenDto } from './dto/refreshToken.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Public()
+  @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
+  async signUp(@Body() authDto: AuthDto): Promise<User> {
+    return await this.authService.singUp(authDto);
+  }
+
+  @Public()
+  @Post('login')
+  @HttpCode(HttpStatus.CREATED)
+  async logIn(@Body() authDto: AuthDto): Promise<Auth> {
+    return await this.authService.logIn(authDto);
+  }
+
+  @Public()
+  @Post('refresh')
+  @HttpCode(HttpStatus.OK)
+  @UsePipes(new ValidationTokenPipe())
+  async refresh(@Body() refreshTokenDto: RefreshTokenDto): Promise<Auth> {
+    return await this.authService.refresh(refreshTokenDto);
+  }
+}
